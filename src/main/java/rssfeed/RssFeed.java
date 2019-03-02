19bfbfc727090ba8898e4fe2b3f8c0a98b646fe5
ExplodingPotato <wojtek4p4@gmail.com>
2019-03-02 13:55:14 +0100

    Added the RssFeed and RssItem classes

---
+++ src/main/java/rssfeed/RssFeed.java
@@ -0,0 +1,13 @@
+package rssfeed;
+
+import java.util.ArrayList;
+
+public class RssFeed {
+    public String title;
+    public String description;
+    public String link;
+    public String language;
+    public String copyright;
+
+    public ArrayList<RssItem> items;
+}
---
+++ src/main/java/rssfeed/RssItem.java
@@ -0,0 +1,13 @@
+package rssfeed;
+
+import java.util.Date;
+
+public class RssItem {
+    public String title;
+    public String link;
+    public String description;
+    public String imgUrl;
+    public Date pubDate;
+
+    public String guid;
+}
