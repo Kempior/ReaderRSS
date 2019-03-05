6b56385f1d7cb5a1cd0ec3d0a99dacd150d35d47
ExplodingPotato <wojtek4p4@gmail.com>
2019-03-05 18:03:20 +0100

    Formatting changes

--- src/test/java/dbaccess/DbAccessorTest.java
+++ src/test/java/dbaccess/DbAccessorTest.java
@@ -10,126 +10,126 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DbAccessorTest {
 
-    private DbAccessor accessor;
-    private RssItem itemNull;
-    private RssItem itemDefault;
-    private RssItem itemModified;
-    private RssItem itemDummy;
+	private DbAccessor accessor;
+	private RssItem itemNull;
+	private RssItem itemDefault;
+	private RssItem itemModified;
+	private RssItem itemDummy;
 
-    @BeforeEach
-    void setUp() {
+	@BeforeEach
+	void setUp() {
 
-        accessor = new DbAccessor("localhost");
-        accessor.switchToDb("test");
+		accessor = new DbAccessor("localhost");
+		accessor.switchToDb("test");
 
-        itemNull = null;
+		itemNull = null;
 
-        itemDefault = new RssItem();
-        itemDefault.guid = "itemDefault";
+		itemDefault = new RssItem();
+		itemDefault.guid = "itemDefault";
 
-        itemModified = new RssItem();
-        itemModified.guid = "itemModified";
-        itemModified.title = "AAA";
-        itemModified.pubDate.setTime(12345);
+		itemModified = new RssItem();
+		itemModified.guid = "itemModified";
+		itemModified.title = "AAA";
+		itemModified.pubDate.setTime(12345);
 
-        itemDummy = new RssItem();
-        itemDummy.guid = "This is the guid";
-        itemDummy.title = "This is the title";
-        itemDummy.description = "This is the description";
-        itemDummy.link = "This is the link";
-        itemDummy.imgUrl = "This is the imgUrl";
-        itemDefault.pubDate.setTime(12345);
-    }
+		itemDummy = new RssItem();
+		itemDummy.guid = "This is the guid";
+		itemDummy.title = "This is the title";
+		itemDummy.description = "This is the description";
+		itemDummy.link = "This is the link";
+		itemDummy.imgUrl = "This is the imgUrl";
+		itemDefault.pubDate.setTime(12345);
+	}
 
-    @AfterEach
-    void tearDown() {
+	@AfterEach
+	void tearDown() {
 
-        accessor.switchToDb("test");
-        accessor.dropDb();
-    }
+		accessor.switchToDb("test");
+		accessor.dropDb();
+	}
 
-    @Test
-    void constructorTest() {
+	@Test
+	void constructorTest() {
 
-        DbAccessor testedAccessor = new DbAccessor("localhost");
-        DbAccessor finalTestedAccessor1 = testedAccessor;
-        assertDoesNotThrow(() -> finalTestedAccessor1.switchToDb("test"));
-        assertTrue(testedAccessor.isConnected());
+		DbAccessor testedAccessor = new DbAccessor("localhost");
+		DbAccessor finalTestedAccessor1 = testedAccessor;
+		assertDoesNotThrow(() -> finalTestedAccessor1.switchToDb("test"));
+		assertTrue(testedAccessor.isConnected());
 
-        testedAccessor = new DbAccessor("This will fail.");
-        testedAccessor.switchToDb("test");
-        assertFalse(testedAccessor.isConnected());
+		testedAccessor = new DbAccessor("This will fail.");
+		testedAccessor.switchToDb("test");
+		assertFalse(testedAccessor.isConnected());
 
-        DbAccessor finalTestedAccessor = testedAccessor;
-        assertThrows(IllegalArgumentException.class, () -> finalTestedAccessor.switchToDb("/\\. \"$*<>:|?"));
-    }
+		DbAccessor finalTestedAccessor = testedAccessor;
+		assertThrows(IllegalArgumentException.class, () -> finalTestedAccessor.switchToDb("/\\. \"$*<>:|?"));
+	}
 
-    @Test
-    void databaseSwitchTest() {
+	@Test
+	void databaseSwitchTest() {
 
-        assertEquals(accessor.getCurrentDb(), "test");
-        assertDoesNotThrow(() -> accessor.switchToDb("testDb"));
+		assertEquals(accessor.getCurrentDb(), "test");
+		assertDoesNotThrow(() -> accessor.switchToDb("testDb"));
 
-        assertEquals(accessor.getCurrentDb(), "testDb");
-    }
+		assertEquals(accessor.getCurrentDb(), "testDb");
+	}
 
-    @Test
-    void inputSimpleTest() {
+	@Test
+	void inputSimpleTest() {
 
-        assertThrows(IllegalArgumentException.class, () -> accessor.add("testFeed", itemNull));
-        assertDoesNotThrow(() -> accessor.add("testFeed", itemModified));
+		assertThrows(IllegalArgumentException.class, () -> accessor.add("testFeed", itemNull));
+		assertDoesNotThrow(() -> accessor.add("testFeed", itemModified));
 
-        assertEquals(itemModified, accessor.find("testFeed").items.get(0));
-        assertEquals(1, accessor.find("testFeed").items.size());
-        assertEquals(0, accessor.find("testFeed").items.size());
-    }
+		assertEquals(itemModified, accessor.find("testFeed").items.get(0));
+		assertEquals(1, accessor.find("testFeed").items.size());
+		assertEquals(0, accessor.find("testFeed").items.size());
+	}
 
-    @Test
-    void inputUnorderedTest() {
+	@Test
+	void inputUnorderedTest() {
 
-        RssFeed feed = new RssFeed();
-        feed.title = "testFeed";
-        feed.items.add(itemDefault);
-        feed.items.add(itemModified);
-        accessor.add(feed);
+		RssFeed feed = new RssFeed();
+		feed.title = "testFeed";
+		feed.items.add(itemDefault);
+		feed.items.add(itemModified);
+		accessor.add(feed);
 
-        RssFeed dbFeed = accessor.find("testFeed");
-        for (RssItem item : dbFeed.items) {
-            assertTrue(feed.items.contains(item));
-        }
-        for (RssItem item : feed.items) {
-            assertTrue(dbFeed.items.contains(item));
-        }
+		RssFeed dbFeed = accessor.find("testFeed");
+		for (RssItem item : dbFeed.items) {
+			assertTrue(feed.items.contains(item));
+		}
+		for (RssItem item : feed.items) {
+			assertTrue(dbFeed.items.contains(item));
+		}
 
-        feed = new RssFeed();
-        dbFeed = accessor.find("testFeed");
+		feed = new RssFeed();
+		dbFeed = accessor.find("testFeed");
 
-        boolean areAllItemsIdentical = true;
-        for (RssItem item : dbFeed.items)
-            if (!feed.items.contains(item))
-                areAllItemsIdentical = false;
-        assertFalse(areAllItemsIdentical);
+		boolean areAllItemsIdentical = true;
+		for (RssItem item : dbFeed.items)
+			if (!feed.items.contains(item))
+				areAllItemsIdentical = false;
+		assertFalse(areAllItemsIdentical);
 
-        areAllItemsIdentical = true;
-        for (RssItem item : dbFeed.items)
-            if (!feed.items.contains(item))
-                areAllItemsIdentical = false;
-        assertFalse(areAllItemsIdentical);
-    }
+		areAllItemsIdentical = true;
+		for (RssItem item : dbFeed.items)
+			if (!feed.items.contains(item))
+				areAllItemsIdentical = false;
+		assertFalse(areAllItemsIdentical);
+	}
 
-    @Test
-    void feedAccessTest() {
-        RssFeed feed = new RssFeed();
-        feed.title = "Feed title";
-        feed.items.add(itemNull);
-        feed.items.add(itemDefault);
-        feed.items.add(itemModified);
-        feed.items.add(itemDummy);
+	@Test
+	void feedAccessTest() {
+		RssFeed feed = new RssFeed();
+		feed.title = "Feed title";
+		feed.items.add(itemNull);
+		feed.items.add(itemDefault);
+		feed.items.add(itemModified);
+		feed.items.add(itemDummy);
 
-        assertNotEquals(feed, accessor.find("Feed title"));
+		assertNotEquals(feed, accessor.find("Feed title"));
 
-        accessor.add(feed);
+		accessor.add(feed);
 
-        assertEquals(feed, accessor.find("Feed title"));
-    }
+		assertEquals(feed, accessor.find("Feed title"));
+	}
 }
